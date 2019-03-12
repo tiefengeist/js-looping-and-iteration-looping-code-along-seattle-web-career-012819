c6e4531fdc294b6894884a4ae73a89f98a85ebbf
Sahir Nambiar <sahirnambiar@gmail.com>
2019-03-12 06:29:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function tailsNeverFails(){
+  let count = 0
+  while(Math.random() >= 0.5) {
+    count++
+  }
+  return `You got ${count} tails in a row!`
+}
+function printBadges(badges){
+  for(let i = 0; i < badges.length; i++){
+    console.log(`Welcome ${badges[i]}! You are employee #${i + 1}.`);
+  }
+  return badges
+}
